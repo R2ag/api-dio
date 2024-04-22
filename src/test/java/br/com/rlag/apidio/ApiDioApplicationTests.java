652b78ed6a38af6c5744c38e9a8841bc2da178ec
Rafael Lacerda <rlag@outlook.com>
2024-04-22 20:21:13 -0300

    commit inicial

---
+++ src/test/java/br/com/rlag/apidio/ApiDioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rlag.apidio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
